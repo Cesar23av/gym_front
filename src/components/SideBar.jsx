3796eed4abce8635f6898dde1ec7912886690dee
Fernando21450 <fernandguzma21@gmail.com>
2026-03-29 16:03:08 -0400

    se agregaron plantillas tailwind para los estilos

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,6 +10,11 @@ const SideBar = () => {
           <li>Membresias</li>
           <li>Reportes</li>
         </ul>
+        <div>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Cerrar Sesión
+          </button>
+        </div>
       </div>
     </>
   );
