fa2b432c5f09c26d9094e6bf05627734911f2d11
Fernando21450 <fernandguzma21@gmail.com>
2026-03-29 01:39:10 -0400

    dashboard y sidebar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,17 @@
+const SideBar = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <h2>Gym Managment</h2>
+        <ul>
+          <li>Usuarios</li>
+          <li>Asistencias</li>
+          <li>Inventario</li>
+          <li>Membresias</li>
+          <li>Reportes</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default SideBar;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+const Dashboard = () => {
+  return (
+    <div className="content">
+      <SideBar />
+      <div className="container">
+        <h1>Dashboard 1</h1>
+      </div>
+    </div>
+  );
+};
+export default Dashboard;
